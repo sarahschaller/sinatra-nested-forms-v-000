3b748dd520f3990b7c39c2750e61e299f4ab1d6b
Sarah Schaller <sarcsch@aol.com>
2017-10-10 15:23:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,12 +16,12 @@ module FormsLab
     end
 
     post '/pirates' do
-      # @pirate = Pirate.new(params[:pirate])
-      #
-      # params[:pirate][:ships].each do |details|
-      #   Ship.new(details)
-      # end
-      # @ships = Ship.all
+      @pirate = Pirate.new(params[:pirate])
+      
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
 
       erb :'pirates/show'
     end
